acbf5d4501051b6d3c3e0d1ab753efa22fed9988
Ekku Sipilä <ekku.sipila@bonsky.com>
2020-10-03 18:27:55 +0300

    Add SoundAnalyzer base project

---
+++ SoundAnalyzer/SoundAnalyzer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+public class SoundAnalyzer : Game
+{
+    public override void Begin()
+    {
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}
+
